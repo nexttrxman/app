690568c9218c0edb6e48ba0973f3f977890c4cab
nexttrxman <tkexchange@proton.me>
2026-09-07 12:41:46 -0300

    Expose PackWizard component globally
    Expose PackWizard component globally on the window object.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/index.css";
 import App from "@/App";
+import { PackWizard } from "@/components/PackWizard";
+window.PackWizard = PackWizard;
 
 const queryClient = new QueryClient({
   defaultOptions: {

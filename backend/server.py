f0e7d1251422bbdaed8c36c159f7e1f7ef6ec5c7
nexttrxman <tkexchange@proton.me>
2026-09-07 03:04:54 -0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -25,10 +25,7 @@ client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
 
 # ---------------- Cloudinary ----------------
-cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+cloudinary.config(url=os.environ.get('CLOUDINARY_URL'))
 )
 
 app = FastAPI()

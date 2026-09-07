e128bea88ef3dd97e629b0d6c720282207822dac
nexttrxman <tkexchange@proton.me>
2026-09-07 13:01:45 -0300

    Change import of PackWizard to default import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/index.css";
 import App from "@/App";
-import { PackWizard } from "@/components/PackWizard";
+import PackWizard from "@/components/PackWizard";
 window.PackWizard = PackWizard;
 
 const queryClient = new QueryClient({
